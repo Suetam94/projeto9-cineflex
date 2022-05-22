28887d342d170855ecf57e8ab2f6536082c6133f
Mateus Vinicius da Silva <mateusviniciusdasilva@outlook.com>
2022-05-22 16:03:26 -0300

    initial files and packages

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  max-width: 375px;
+  height: 67px;
+  background: #c3cfd9;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  width: 100%;
+  top: 0;
+  left: 0;
+
+  h1 {
+    font-family: "Roboto", sans-serif;
+    font-weight: 400;
+    font-size: 34px;
+    line-height: 40px;
+    color: #e8833a;
+    text-transform: uppercase;
+  }
+`;
